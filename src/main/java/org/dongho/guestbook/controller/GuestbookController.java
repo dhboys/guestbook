e670a11d4865dc0bf28c7d88054bdcef590b62b8
dongho YUN <76724739+dhboys@users.noreply.github.com>
2021-04-16 11:43:14 +0900

    commit..

--- src/main/java/org/dongho/guestbook/controller/GuestbookController.java
+++ src/main/java/org/dongho/guestbook/controller/GuestbookController.java
@@ -1,20 +1,39 @@
 package org.dongho.guestbook.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.dongho.guestbook.dto.PageRequestDTO;
+import org.dongho.guestbook.service.GuestbookService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/guestbook")
 @Log4j2
+@RequiredArgsConstructor
 public class GuestbookController {
-    @GetMapping({"/" , "/list"})
-    public String list(){
 
-        log.info("list..........");
+    private final GuestbookService serivce;
+
+    @GetMapping({"/" , "/list"})
+    public String list(PageRequestDTO pageRequestDTO , Model model){
+
+        log.info("list.........." + pageRequestDTO);
+
+        log.info("---------------------------");
+        log.info(serivce.getList(pageRequestDTO));
+
+        model.addAttribute("list" , serivce.getList(pageRequestDTO));
 
         return "/guestbook/list";
     }
+
+    @GetMapping("/register")
+    public void register(){
+        log.info("register...");
+    }
+
 
 }
